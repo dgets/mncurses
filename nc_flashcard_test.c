ae55cf45e975a36dd930778a108b40970115ec91
Damon Getsman <damo.gets@gmail.com>
2015-01-28 20:59:48 -0800

    Started testing code, predecessor to flashcards for lil D

---
+++ nc_flashcard_test.c
@@ -0,0 +1,18 @@
+/*
+ * started: 28jan15
+ * finished:
+ * written by: Damon Getsman
+ *
+ * This project is a simple test to make sure that the different functions
+ * available in my-nc_flashcard.h are working properly; a predecessor to the
+ * flashcard program that I am writing for my son, so that he can be able
+ * to practice his multiplication on anything from a text-only terminal to
+ * a terminal on any machine in the house.  It seems like so many of the other
+ * programs that I've used are buggy...  I figured, why not put this library
+ * to use, get more familiar with C again, and code him up something simple
+ * and quick that'll get the job done.
+ */
+
+#include "my-nc_flashcard.h"
+
+
